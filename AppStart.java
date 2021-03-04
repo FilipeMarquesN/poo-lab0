66a7e312400414ee7c9414dd15a264009d0f007e
FilipeMarquesN <200221035@estudantes.ips.pt>
2021-03-04 16:14:19 +0000

    Change

--- AppStart.java
+++ AppStart.java
@@ -16,7 +16,7 @@ public class AppStart
         System.out.println ("M - Médio");
         System.out.println ("L - Grande");
         String tamanho = scan.next();
-        int numeroDeCopos = 0;
+     
         if(tamanho == "s"){
             tamanho = "S";}else if(tamanho == "m"){
                 tamanho = "M";}else if(tamanho == "l"){
@@ -25,28 +25,28 @@ public class AppStart
             
         switch (tamanho) {
             case "S":
-                numeroDeCopos+=1;
+             
                 System.out.println("Name         : " + Size.SMALL);
                 System.out.println("toString()   : " + Size.SMALL.toString());
-                System.out.println("Ordem        : " + numeroDeCopos);
+                System.out.println("Ordem        : " + Size.SMALL.ordinal());
                 System.out.println("Valor minimo : " + Size.SMALL.getMinValue());
                 System.out.println("Valor máximo : " + Size.SMALL.getMinValue());
                 System.out.println("Código       : " + Size.SMALL.getCode());
                 break;
             case "M":
-                numeroDeCopos+=1;
+          
                 System.out.println("Name         : " + Size.MEDIUM);
                 System.out.println("toString()   : " + Size.MEDIUM.toString());
-                System.out.println("Ordem        : " + numeroDeCopos);
+                System.out.println("Ordem        : " + Size.MEDIUM.ordinal());
                 System.out.println("Valor minimo : " + Size.MEDIUM.getMinValue());
                 System.out.println("Valor máximo : " + Size.MEDIUM.getMinValue());
                 System.out.println("Código       : " + Size.MEDIUM.getCode());
                 break;
             case "L":
-                numeroDeCopos+=1;
+              
                 System.out.println("Name         : " + Size.LARGE);
                 System.out.println("toString()   : " + Size.LARGE.toString());
-                System.out.println("Ordem        : " + numeroDeCopos);
+                System.out.println("Ordem        : " + Size.LARGE.ordinal());
                 System.out.println("Valor minimo : " + Size.LARGE.getMinValue());
                 System.out.println("Valor máximo : " + Size.LARGE.getMinValue());
                 System.out.println("Código       : " + Size.LARGE.getCode());
